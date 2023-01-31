7de8a419f4ce7e62f78c5a951ee85ef5d0c2e55d
shnasta <shvets.nasta@gmail.com>
2023-01-31 18:41:00 +0200

    Added Player class and moving features

--- main.lua
+++ main.lua
@@ -11,37 +11,12 @@ function love.load()
         resizable = true
     })
 
-    love.keyboard.keysPressed = {}
-    love.mouse.buttonsPressed = {}
+
+    player = Player()
 end
-
-function love.resize(w, h)
-    push:resize(w, h)
-end
-
-function love.keypressed(key)
-    -- add to our table of keys pressed this frame
-    love.keyboard.keysPressed[key] = true
-
-    if key == 'escape' then
-        love.event.quit()
-    end
-end
-
-function love.mousepressed(_, _, button)
-    love.mouse.buttonsPressed[button] = true
-end
-
-function love.keyboard.wasPressed(key)
-    return love.keyboard.keysPressed[key]
-end
-
-function love.mouse.wasPressed(button)
-    return love.mouse.buttonsPressed[button]
-end
-
 
 function love.update(dt)
+    player:update(dt)
 
     love.keyboard.keysPressed = {}
     love.mouse.buttonsPressed = {}
@@ -52,6 +27,7 @@ function love.draw()
 
     love.graphics.draw(BACKGROUND_IMAGE, 0, 0)
 
+    player:render()
 
     push:finish()
 end
--- src/dependencies.lua
+++ src/dependencies.lua
@@ -2,3 +2,8 @@
 require 'src/constants'
 
 push = require 'libraries/push'
+anim8 = require 'libraries/anim8'
+Class = require 'libraries/class'
+
+
+require 'src/Player'
