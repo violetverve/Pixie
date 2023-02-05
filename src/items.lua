3535f49da033cca62b10e2b503d56773af86bb91
YarynaFialko <92580927+YarynaFialko@users.noreply.github.com>
2023-02-05 23:12:04 +0200

    fruits added to the table

--- src/items.lua
+++ src/items.lua
@@ -6,5 +6,54 @@ ITEMS_DEFS = {
         image = love.graphics.newImage('images/items/apple_red.png'),
         colliderType = 'circle',
         type = 'apple'
-    }
+    },
+    ['corn'] = {
+        width = 16,
+        height = 16,
+        image = love.graphics.newImage('images/items/corn.png'),
+        colliderType = 'circle',
+        type = 'corn'
+    },
+    ['orange'] = {
+        width = 16,
+        height = 16,
+        image = love.graphics.newImage('images/items/orange.png'),
+        colliderType = 'circle',
+        type = 'orange'
+    },
+    ['pear'] = {
+        width = 16,
+        height = 16,
+        image = love.graphics.newImage('images/items/pear.png'),
+        colliderType = 'circle',
+        type = 'pear'
+    },
+    ['pomegranate'] = {
+        width = 16,
+        height = 16,
+        image = love.graphics.newImage('images/items/pomegranate.png'),
+        colliderType = 'circle',
+        type = 'pomegranate'
+    },
+    ['strawberry'] = {
+        width = 16,
+        height = 16,
+        image = love.graphics.newImage('images/items/strawberry.png'),
+        colliderType = 'circle',
+        type = 'pear'
+    },
+    ['tomato'] = {
+        width = 16,
+        height = 16,
+        image = love.graphics.newImage('images/items/tomato.png'),
+        colliderType = 'circle',
+        type = 'tomato'
+    },
+    ['watermelon'] = {
+        width = 16,
+        height = 16,
+        image = love.graphics.newImage('images/items/watermelon_whole.png'),
+        colliderType = 'circle',
+        type = 'watermelon'
+    },
 }
